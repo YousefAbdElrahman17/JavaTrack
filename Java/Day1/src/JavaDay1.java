a8a55f562f2006185ea9a3f98b9e62c4e16f7ebe
YousefAbdElrahman17 <jolmera2003@gmail.com>
2026-09-21 20:02:36 +0300

    Adding Java Day1 Lab

--- Java/Day1/src/JavaDay1.java
+++ Java/Day1/src/JavaDay1.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
-public class Day1Main{
+public class JavaDay1 {
     public static void main(String[] args){
 
         Scanner in = new Scanner(System.in);
 
+        //region 1. Mini-Calculator
         System.out.print("Enter Number1: ");
         int A = Integer.parseInt(in.nextLine());
         System.out.print("Enter Number2: ");
@@ -15,6 +16,15 @@ public class Day1Main{
         System.out.println(A + " * " + B +" = "+ (A*B));
         System.out.println(B!=0? A + " / " + B +" = " +(A/B):"Invalid");
         System.out.println(A + " % " + B +" = "+ (A%B));
+        // endregion
+
+        System.out.println("--\n-----------------------------\n--");
+
+        //region 2. Last-Digit
+        System.out.print("Enter a number: ");
+        int num = Integer.parseInt(in.nextLine());
+        System.out.println(num%2==0? 1:6);
+        // endregion
 
     }
 }
